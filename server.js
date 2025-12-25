68f9a4f04790c5484ac2f9147f88866655a609f8
Hubaal-ops <hubaalcorportion@gmail.com>
2025-12-25 16:46:44 +0300

    cron.js added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import job from './src/config/cron.js';
 
 import { connectDB } from './src/config/db.js'
 import authRoutes from './src/routes/authRoutes.js'
@@ -11,17 +12,17 @@ dotenv.config()
 
 
 const app = express()
-
 const PORT = process.env.PORT || 8000
-
+job.start()
 
 app.use(cors())
 app.use(express.json())
 
+
 app.use('/api/users', authRoutes)
 app.use('/api/posts', postRoutes)
 
-
+cron
 
 app.listen(PORT, () => {
     console.log(`The app is running on port ${PORT}`,
